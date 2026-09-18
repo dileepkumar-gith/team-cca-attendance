e223958425b2eb625478a999f7035ba180116a33
Dileep kumar Muttavarapu <mdileepkumar826@gmail.com>
2026-09-18 17:23:11 +0530

    Add CSS styling, mobile viewport meta tag, and fix table horizontal scroll on mobile

--- ccaattendance/settings.py
+++ ccaattendance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4sjp62#c7#n96%*(6q683ming7!rylnp(*0ua_z%st(yha2a%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
